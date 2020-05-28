23502c1b61cb75ea74ff64470ca9a7886f7eb45a
Juan Andrade <49418565+juan-20@users.noreply.github.com>
2020-05-28 15:54:13 -0300

    commit

---
+++ Atleta/Atleta/Tela.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frm_cadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_resultado = new System.Windows.Forms.Label();
+            this.txt_resposta = new System.Windows.Forms.TextBox();
+            this.txt_idade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_peso = new System.Windows.Forms.TextBox();
+            this.lbl_peso = new System.Windows.Forms.Label();
+            this.txt_altura = new System.Windows.Forms.TextBox();
+            this.lbl_altura = new System.Windows.Forms.Label();
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.lbl_nome = new System.Windows.Forms.Label();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_resultado
+            // 
+            this.lbl_resultado.AutoSize = true;
+            this.lbl_resultado.Location = new System.Drawing.Point(165, 119);
+            this.lbl_resultado.Name = "lbl_resultado";
+            this.lbl_resultado.Size = new System.Drawing.Size(58, 13);
+            this.lbl_resultado.TabIndex = 23;
+            this.lbl_resultado.Text = "Resultado:";
+            // 
+            // txt_resposta
+            // 
+            this.txt_resposta.Location = new System.Drawing.Point(168, 149);
+            this.txt_resposta.Name = "txt_resposta";
+            this.txt_resposta.Size = new System.Drawing.Size(100, 20);
+            this.txt_resposta.TabIndex = 22;
+            // 
+            // txt_idade
+            // 
+            this.txt_idade.Location = new System.Drawing.Point(15, 227);
+            this.txt_idade.Name = "txt_idade";
+            this.txt_idade.Size = new System.Drawing.Size(100, 20);
+            this.txt_idade.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Idade";
+            // 
+            // txt_peso
+            // 
+            this.txt_peso.Location = new System.Drawing.Point(12, 163);
+            this.txt_peso.Name = "txt_peso";
+            this.txt_peso.Size = new System.Drawing.Size(100, 20);
+            this.txt_peso.TabIndex = 19;
+            // 
+            // lbl_peso
+            // 
+            this.lbl_peso.AutoSize = true;
+            this.lbl_peso.Location = new System.Drawing.Point(12, 135);
+            this.lbl_peso.Name = "lbl_peso";
+            this.lbl_peso.Size = new System.Drawing.Size(31, 13);
+            this.lbl_peso.TabIndex = 18;
+            this.lbl_peso.Text = "Peso";
+            // 
+            // txt_altura
+            // 
+            this.txt_altura.Location = new System.Drawing.Point(12, 103);
+            this.txt_altura.Name = "txt_altura";
+            this.txt_altura.Size = new System.Drawing.Size(100, 20);
+            this.txt_altura.TabIndex = 17;
+            // 
+            // lbl_altura
+            // 
+            this.lbl_altura.AutoSize = true;
+            this.lbl_altura.Location = new System.Drawing.Point(12, 75);
+            this.lbl_altura.Name = "lbl_altura";
+            this.lbl_altura.Size = new System.Drawing.Size(34, 13);
+            this.lbl_altura.TabIndex = 16;
+            this.lbl_altura.Text = "Altura";
+            // 
+            // txt_nome
+            // 
+            this.txt_nome.Location = new System.Drawing.Point(12, 39);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(100, 20);
+            this.txt_nome.TabIndex = 15;
+            // 
+            // lbl_nome
+            // 
+            this.lbl_nome.AutoSize = true;
+            this.lbl_nome.Location = new System.Drawing.Point(9, 9);
+            this.lbl_nome.Name = "lbl_nome";
+            this.lbl_nome.Size = new System.Drawing.Size(35, 13);
+            this.lbl_nome.TabIndex = 14;
+            this.lbl_nome.Text = "Nome";
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(11, 264);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(101, 23);
+            this.btn_calcular.TabIndex = 13;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // frm_cadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(303, 299);
+            this.Controls.Add(this.lbl_resultado);
+            this.Controls.Add(this.txt_resposta);
+            this.Controls.Add(this.txt_idade);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_peso);
+            this.Controls.Add(this.lbl_peso);
+            this.Controls.Add(this.txt_altura);
+            this.Controls.Add(this.lbl_altura);
+            this.Controls.Add(this.txt_nome);
+            this.Controls.Add(this.lbl_nome);
+            this.Controls.Add(this.btn_calcular);
+            this.Name = "frm_cadastro";
+            this.Text = "Tela";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_resultado;
+        private System.Windows.Forms.TextBox txt_resposta;
+        private System.Windows.Forms.TextBox txt_idade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_peso;
+        private System.Windows.Forms.Label lbl_peso;
+        private System.Windows.Forms.TextBox txt_altura;
+        private System.Windows.Forms.Label lbl_altura;
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.Label lbl_nome;
+        private System.Windows.Forms.Button btn_calcular;
+    }
+}
---
+++ Atleta/Atleta/Tela.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frm_cadastro : Form
+    {
+        public frm_cadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+
+        private void lbl_nome_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Atleta novoAtleta = new Atleta();
+
+                novoAtleta.Nome = txt_nome.Text;
+                novoAtleta.Idade = int.Parse(txt_idade.Text);
+                novoAtleta.Altura = double.Parse(txt_altura.Text);
+                novoAtleta.Peso = double.Parse(txt_peso.Text);
+                txt_resposta.Text = novoAtleta.CalcularIMC().ToString();
+            }
+            catch (FormatException exeception)
+            {
+                MessageBox.Show("Erro. Caixa vazia ou no formato errado.  \n" + exeception.Message);
+            }
+            catch (Exception exeception)
+            {
+                MessageBox.Show(exeception.Message);
+            }
+
+
+        }
+
+        private void lbl_resultado_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
